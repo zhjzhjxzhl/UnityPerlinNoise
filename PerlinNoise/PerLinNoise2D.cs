295c234f6b1d5d21e33fdd555b015e62fa411cd0
zhjzhjxzhl <zhjzhjxzhl@163.com>
2017-12-21 15:21:29 +0800

    Add files via upload

---
+++ PerlinNoise/PerLinNoise2D.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// PerLinNoise2D 注释
+/// <para>Author: zhaojun jun.zhao@ifreeteam.com</para>
+/// <para>Date: 2014/5/30 10:26:12</para>
+/// <para>$Id$</para>
+/// </summary>
+
+namespace PerlinNoise
+{
+    class PerLinNoise2D
+    {
+        private static float noise(int x, int y)
+        {
+            int n = x + y * 57;
+            n = (n << 13) ^ n;
+            return (float)( 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0); 
+        }
+
+        private static float smoothNoise(int x, int y)
+        {
+            float corners = (noise(x - 1, y - 1) + noise(x - 1, y + 1) + noise(x + 1, y - 1) + noise(x + 1, y + 1)) / 16;
+            float sides = (noise(x - 1, y) + noise(x + 1, y) + noise(x, y - 1) + noise(x, y + 1)) / 8;
+            float center = noise(x, y) / 4;
+            return (corners + sides + center);
+        }
+
+        private static float interpolate(float x, float y)
+        {
+            int xx = (int)x;
+            int yy = (int)y;
+            float fractional_x = x - xx;
+            float fractional_y = y - yy;
+
+            float v1 = smoothNoise(xx, yy);
+            float v2 = smoothNoise(xx, yy + 1);
+            float v3= smoothNoise(xx + 1, yy);
+            float v4 = smoothNoise(xx + 1, yy + 1);
+
+            float i1 = PerlinNoise.PerlinNoise1D.Cosine_Interpolate(v1, v2, fractional_x);
+            float i2 = PerlinNoise1D.Cosine_Interpolate(v3, v4, fractional_y);
+
+            return PerlinNoise1D.Cosine_Interpolate(i1, i2, Mathf.Sqrt(fractional_y*fractional_x));
+
+        }
+
+        public static float genPerlinNoise(float x,float y)
+        {
+            float total = 0.0f;
+            float persistence = 0.4f;
+            int octaves = 6;
+            for (int i = 0; i < octaves; i++)
+            {
+                float frequency = (1 << i);
+                float amplitude = Mathf.Pow(persistence, i);
+                total += interpolate(x * frequency,y*frequency) * amplitude;
+            }
+            return total;
+        }
+
+    }
+
+    
+}
---
+++ PerlinNoise/PerlinNoise1D.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// PerlinNoise 注释
+/// <para>Author: zhaojun jun.zhao@ifreeteam.com</para>
+/// <para>Date: 2014/5/30 9:50:04</para>
+/// <para>$Id$</para>
+/// </summary>
+
+namespace PerlinNoise
+{
+    class PerlinNoise1D
+    {
+        private static float noise(int x)
+        {
+            x = (x<<13) ^ x;
+            return (float)( 1.0 - ( (x * (x * x * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0d); 
+        }
+
+        private static float smoothNoise(int x)
+        {
+            return noise(x) / 2 + noise(x - 1) / 4 + noise(x + 1) / 4;
+        }
+
+        public static float Cosine_Interpolate(float a, float b, float x)
+        {
+            float ft = x * 3.1415927f;
+            float f = (1 - Mathf.Cos(ft)) * 0.5f;
+            return a * (1 - f) + b * f;
+        }
+
+        private static float InterpolateNoise(float x)
+        {
+            int xx = (int)x;
+            float fractional_x = x-xx;
+            float v1 = smoothNoise(xx);
+            float v2 = smoothNoise(xx+1);
+            return Cosine_Interpolate(v1, v2, fractional_x);
+        }
+
+        public static float genPerlinNoise(float x)
+        {
+            float total = 0.0f;
+            float persistence = 0.8f;
+            int octaves = 6;
+            for (int i = 0; i < octaves; i++)
+            {
+                int frequency = 1 << i;
+                float amplitude = Mathf.Pow(persistence, i);
+                total += InterpolateNoise(x * frequency) * amplitude;
+            }
+            return total;
+        }
+
+    }
+    
+}
